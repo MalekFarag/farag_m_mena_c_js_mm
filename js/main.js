5050983801d82646efbcebc9c8023ed72f21b4a2
MalekFarag <43182938+MalekFarag@users.noreply.github.com>
2019-02-02 22:00:54 -0500

    added files to branch. Created instructions sheet

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//create jar -variable-, name jar, place string in jar
+//var firstName = "Marco";
+
+//functions- method are preexisting fuctions
+//alert("Hello" + firstName);
+
+//modern js
+//alert(`Hello ${firstName}`)
+
+//console.log("Hello" + firstName);
+
+console.log("javascript linked up");
