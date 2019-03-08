39c7b6dc107bc32e7ea25dff88827cfbfef4f48c
MalekFarag <43182938+MalekFarag@users.noreply.github.com>
2019-03-07 20:52:22 -0500

    HTML, CSS added. JS almost completed (need assets to complete)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,44 @@
-//create jar -variable-, name jar, place string in jar
-//var firstName = "Marco";
-
-//functions- method are preexisting fuctions
-//alert("Hello" + firstName);
-
-//modern js
-//alert(`Hello ${firstName}`)
-
-//console.log("Hello" + firstName);
-
+(() => {
 console.log("javascript linked up");
+
+const item = document.querySelectorAll('.draggable')
+
+let dropZone = document.querySelector('.dropZone');
+let dragZone = document.querySelector('.dragZone');
+let resetZone = document.querySelector('.resetZone');
+
+
+
+/////////////////////////////////DRAG////////////////////////////////
+function drag() {
+	dragZone.querySelectorAll('img').forEach(img =>{
+		img.addEventListener('dragstart', function(e){
+			console.log('dragging...');
+			e.dataTransfer.setData('text/plain', this.id);
+		});
+	});
+}
+
+/////////////////////////////RESET BOARD////////////////////////////
+function resetBoard() {
+	dropZone.removeAllFiles();
+}
+
+resetZone.addEventListener('click', resetBoard);
+
+
+/////////////////////////////////DROP/////////////////////////////////
+dropZone.addEventListener('dragover', function(e){
+	e.preventDefault();
+	console.log('drop?')
+});
+
+dropZone.addEventListener('drop', function(e){
+	e.preventDefault();
+	console.log('dropped')
+	item.audio.play();// *********check here for errors
+
+});
+
+
+})();
